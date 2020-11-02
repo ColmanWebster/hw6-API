0d6f7b7bae34b87d341e4ddb3ee50800725e8e1b
Colman Webster <roguefiend@yahoo.com>
2020-11-02 00:01:10 -0800

    touch up

--- scripts/hw6.js
+++ scripts/hw6.js
@@ -5,7 +5,12 @@ var input = document.querySelector('#inputSmall');
 var data;
 var Request;
 var Request2;
+var Request3;
+var coord1;
+var lon;
+var lat;
 var date = new Date();
+    
 
 if (localStorage.getItem('searches')){
     var searches = JSON.parse(localStorage.getItem('searches'));
@@ -16,25 +21,21 @@ if (localStorage.getItem('searches')){
 
 document.querySelector('.cities').innerHTML = searches.join('');
 
-cities.addEventListener('click', function(event){
-    if(event.target.matches('.city')){
-        input.value = event.target.textContent;
-
-        Request2 = new XMLHttpRequest();
+    
+function doTwo(data){
+    Request2 = new XMLHttpRequest();
         Request2.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?q='+ input.value +'&mode=json&appid=41fc8ca598f3a7d7ba154982bb7f9da1&units=imperial');
-        Request2.onload = function(){
+        Request2.onload = () => {
             data = JSON.parse(Request2.responseText);
-
-        //    $('#cityDate').html('<img src="http://openweathermap.org/img/wn/' + data.list[0].weather[0].icon + '@2x.png" width = "16" height = "16"></img></br>');
-            document.querySelectorAll('.card')[0].innerHTML = 'data.city.name + '; ' + date.getDay() + '/' + date.getMonth() + '/' + date.getUTCFullYear()';
+        
+            document.querySelector('#cityDate').innerHTML = data.city.name + '; ' + data.list[0].dt_txt /*date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear();*/
             $('#temp').text(Math.floor(data.list[0].main.temp) + '°');
             $('#hum').text(data.list[0].main.humidity + '%');
             $('#wind').text(data.list[0].wind.speed);
-
-            console.log(data.list[0].weather[0].icon);
+           
 
         for(i=0; i<39;){
-            document.querySelectorAll('.card')[i/8].innerHTML = ('<img src="http://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png" width = "16" height = "16"></img></br>'/*'<img src="http://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png" width = "16" height = "16"></img></br>'*/);
+            document.querySelectorAll('.card')[i/8].innerHTML = (data.list[i].dt_txt+'<img src="http://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png" width = "16" height = "16"></img></br> Temp: '+Math.floor(data.list[i].main.temp)+ '° </br> Hum: ' + data.list[i].main.humidity +'%</br>');
             i = i+8;
             };
 
@@ -47,12 +48,40 @@ cities.addEventListener('click', function(event){
             localStorage.setItem('searches', searches2);
             var searches3 =JSON.parse(localStorage.getItem('searches'));
             document.querySelector('.cities').innerHTML = searches3.join('');
+
+            coord1 = data.city.coord;
+             lat = coord1.lat;
+             lon = coord1.lon;    
+            //console.log(lat);
+            //console.log(lon);
         };
 
         Request2.send();
+        return data;
+};
 
+cities.addEventListener('click', function(event){
+    if(event.target.matches('.city')){
+        input.value = event.target.textContent;
+
+
+    function doThree(hold){
+        var hold = doTwo();
+        Request3 = new XMLHttpRequest();
+        Request3.open('GET', 'http://api.openweathermap.org/data/2.5/uvi?lat=33.875294&lon=-117.566437&mode=json&appid=41fc8ca598f3a7d7ba154982bb7f9da1');
+        Request3.onload = function(){
+            data2 = (Request3.responseText);         
+        };
+        console.log(hold);
+        }
+        doThree();
     }
-});
+    
+    });
+        
+
+    
+
 
 submit.addEventListener('click', function(){
     Request = new XMLHttpRequest();
@@ -60,7 +89,7 @@ submit.addEventListener('click', function(){
     Request.onload = function(){
         data = JSON.parse(Request.responseText);
         for(i=0; i<40;){
-            document.querySelectorAll('.card')[i/8].innerHTML = ('<img src="http://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png" width = "16" height = "16"></img></br>');
+            document.querySelectorAll('.card')[i/8].innerHTML = (data.list[i].dt_txt+'<img src="http://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png" width = "16" height = "16"></img></br> Temp: '+Math.floor(data.list[i].main.temp)+ '° </br> Hum: ' + data.list[i].main.humidity +'%</br>');
             i = i+8;
             };
 
@@ -83,7 +112,7 @@ submit.addEventListener('click', function(){
    
     };
      Request.send();
-
+    return data;
     
 
 });
