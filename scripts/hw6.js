c5aebaa9cda66b828faa952347de59aa3da7e73b
Colman Webster <roguefiend@yahoo.com>
2020-11-04 14:07:31 -0800

    complete functionality

--- scripts/hw6.js
+++ scripts/hw6.js
@@ -3,6 +3,7 @@ var cities = document.querySelector('.cities');
 var submit = document.querySelector('#submit');
 var input = document.querySelector('#inputSmall');
 var data;
+var data2 = {};
 var Request;
 var Request2;
 var Request3;
@@ -22,16 +23,18 @@ if (localStorage.getItem('searches')){
 document.querySelector('.cities').innerHTML = searches.join('');
 
     
-function doTwo(data){
+function doTwo(){
     Request2 = new XMLHttpRequest();
-        Request2.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?q='+ input.value +'&mode=json&appid=41fc8ca598f3a7d7ba154982bb7f9da1&units=imperial');
+        Request2.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?q='+ input.value +'&mode=json&appid=41fc8ca598f3a7d7ba154982bb7f9da1&units=imperial');
         Request2.onload = () => {
             data = JSON.parse(Request2.responseText);
+            //console.log(data);
         
-            document.querySelector('#cityDate').innerHTML = data.city.name + '; ' + data.list[0].dt_txt /*date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear();*/
-            $('#temp').text(Math.floor(data.list[0].main.temp) + '°');
-            $('#hum').text(data.list[0].main.humidity + '%');
-            $('#wind').text(data.list[0].wind.speed);
+            //document.querySelector('#cityDate').innerHTML = data.city.name + '; ' + data.list[0].dt_txt /*date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear();*/
+            $('#cityDate').text(data.city.name + '  ; ' + date.getDay() + '/' + date.getMonth() + '/' + date.getUTCFullYear());
+            $('#temp').text('Temperature: ' + Math.floor(data.list[0].main.temp) + '°');
+            $('#hum').text('Humidity: ' + data.list[0].main.humidity + '%');
+            $('#wind').text('Wind Speed: ' + data.list[0].wind.speed);
            
 
         for(i=0; i<39;){
@@ -50,42 +53,39 @@ function doTwo(data){
             document.querySelector('.cities').innerHTML = searches3.join('');
 
             coord1 = data.city.coord;
-             lat = coord1.lat;
-             lon = coord1.lon;    
-            //console.log(lat);
-            //console.log(lon);
-        };
-
+            lat = coord1.lat;
+            lon = coord1.lon;    
+            
+            
+             
+            Request3 = new XMLHttpRequest();
+            Request3.open('GET', 'https://api.openweathermap.org/data/2.5/uvi?lat='+ coord1.lat +'&lon='+coord1.lon+'&appid=41fc8ca598f3a7d7ba154982bb7f9da1');
+            
+            Request3.onload = () => {
+                data2 = (Request3.responseText);
+                //console.log(JSON.parse(data2).value);
+                $('#uvi').text('UV Index: ' + JSON.parse(data2).value);
+            };
+            Request3.send();
+            
+        }
         Request2.send();
-        return data;
+        
 };
 
 cities.addEventListener('click', function(event){
     if(event.target.matches('.city')){
         input.value = event.target.textContent;
 
-
-    function doThree(hold){
-        var hold = doTwo();
-        Request3 = new XMLHttpRequest();
-        Request3.open('GET', 'http://api.openweathermap.org/data/2.5/uvi?lat=33.875294&lon=-117.566437&mode=json&appid=41fc8ca598f3a7d7ba154982bb7f9da1');
-        Request3.onload = function(){
-            data2 = (Request3.responseText);         
-        };
-        console.log(hold);
-        }
-        doThree();
-    }
-    
-    });
+    doTwo();
         
 
-    
-
+    }
+});
 
 submit.addEventListener('click', function(){
     Request = new XMLHttpRequest();
-    Request.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?q='+ input.value +'&mode=json&appid=41fc8ca598f3a7d7ba154982bb7f9da1&units=imperial');
+    Request.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?q='+ input.value +'&mode=json&appid=41fc8ca598f3a7d7ba154982bb7f9da1&units=imperial');
     Request.onload = function(){
         data = JSON.parse(Request.responseText);
         for(i=0; i<40;){
@@ -94,9 +94,9 @@ submit.addEventListener('click', function(){
             };
 
             $('#cityDate').text(data.city.name + '  ; ' + date.getDay() + '/' + date.getMonth() + '/' + date.getUTCFullYear());
-            $('#temp').text(Math.floor(data.list[0].main.temp) + '°');
-            $('#hum').text(data.list[0].main.humidity + '%');
-            $('#wind').text(data.list[0].wind.speed);
+            $('#temp').text('Temperature: ' + Math.floor(data.list[0].main.temp) + '°');
+            $('#hum').text('Humidity: ' + data.list[0].main.humidity + '%');
+            $('#wind').text('Wind Speed: ' + data.list[0].wind.speed);
 
             searches.push('<button class = "city">'+ input.value + '</button></br>');
            
@@ -109,15 +109,29 @@ submit.addEventListener('click', function(){
             document.querySelector('.cities').innerHTML = searches3.join('');
             //'<button>'+ input.value + '</button></br>';
             
+            coord1 = data.city.coord;
+            lat = coord1.lat;
+            lon = coord1.lon;    
+            
+            
+             
+            Request3 = new XMLHttpRequest();
+            Request3.open('GET', 'https://api.openweathermap.org/data/2.5/uvi?lat='+ coord1.lat +'&lon='+coord1.lon+'&appid=41fc8ca598f3a7d7ba154982bb7f9da1');
+            
+            Request3.onload = () => {
+                data2 = (Request3.responseText);
+                //console.log(JSON.parse(data2).value);
+                $('#uvi').text('UV Index: ' + JSON.parse(data2).value);
+            };
+            Request3.send();
+
    
     };
      Request.send();
-    return data;
+
     
 
 });
-
-
 
 
 
